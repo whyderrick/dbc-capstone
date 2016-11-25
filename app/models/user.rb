1bae2cd262907ac7ce00a20bf73a1bb6313e5301
Jules Martin <julesmartin76000@gmail.com>
2016-11-24 20:21:37 -0800

    sessions//users are ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   # has_many :groups, through: :user_groups
   has_many :requested_walks, foreign_key: :requester_id, class_name: 'Walk'
   has_many :guarded_walks, foreign_key: :guardian_id, class_name: 'Walk'
-	validates :username, :email, :password_digest, :verified, { presence: :true }
+	validates :username, :email, :password_digest, { presence: :true }
 end
