eedee2eef1f51e00629a2fd65724dac43a9982b7
Derrick Carr <derrick.the.future@gmail.com>
2016-11-24 22:31:28 -0800

    Add attribute tests for user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user){User.new(username: 'derrick', email: 'derrick@derrick.com', password: 'aaa', verified: true )}
+  describe "it has attributes" do
+    it "has a username" do
+      expect( user.username ).to_not eq nil
+      expect( user.username ).to eq 'derrick'
+    end
+
+    it "has an email" do
+      expect( user.email ).to_not eq nil
+      expect( user.email ).to eq 'derrick@derrick.com'
+    end
+
+    it "has a password_digest" do
+      expect( user.password_digest ).to_not eq nil
+      expect( user.password_digest ).to eq 'aaa'
+    end
+  end
 end
