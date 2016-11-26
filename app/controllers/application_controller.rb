edd483667446af08a9fa8c5380e4b930b2d772a2
Derrick Carr <derrick.the.future@gmail.com>
2016-11-26 15:01:55 -0800

    Change walks association to guardian from belongs_to to has_one

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_user, :login, :authorize
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
 
 
   def login
@@ -14,8 +15,8 @@ class ApplicationController < ActionController::Base
   def authorize
     unless current_user
     	flash[:notice] = "Please login to access that information!"
-    	redirect_to '/login' 
+    	redirect_to '/login'
     end
   end
-  
+
 end
