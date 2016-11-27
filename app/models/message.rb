1aedb9fccafad70973700bde56b1fb681699861b
DL804 <darren804@gmail.com>
2016-11-27 15:47:56 -0800

    modified migrations, ok

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ApplicationRecord
-  after_create_commit { MessageBroadcastJob.perform_later self }
+  # after_create_commit { MessageBroadcastJob.perform_later self }
+  belongs_to :chatroom
+  belongs_to :user
 end
--- db/migrate/20161125052019_create_messages.rb
+++ db/migrate/20161125052019_create_messages.rb
@@ -1,8 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.text :content
-
+      t.string :content
+      t.references :user, index: true, foreign_key: true
+      t.references :chatroom, index: true, foreign_key: true
       t.timestamps
     end
   end
