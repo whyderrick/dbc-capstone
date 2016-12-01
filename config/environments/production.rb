74bcf321f7c083ed9f257b567e053dac35c53d31
DL804 <darren804@gmail.com>
2016-12-01 11:09:04 -0800

    add route to dbc.net

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,9 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
   # actioncable setting for deployment to Heroku, default action is set to localhost
-  config.web_socket_server_url = "wss://safewalk-dbc.herokuapp.com/cable" 
+  config.web_socket_server_url = "wss://www.safewalk-dbc.net/cable" 
   config.action_cable.allowed_request_origins = ["http://www.safewalk-dbc.net"]
-    config.action_cable.allowed_request_origins = ["https://safewalk-dbc.herokuapp.com"]
+    # config.action_cable.allowed_request_origins = ["https://safewalk-dbc.herokuapp.com"]
 
   
   # Code is not reloaded between requests.
