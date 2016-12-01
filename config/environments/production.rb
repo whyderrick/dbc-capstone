dbf8bbba088ef7425605e122e3de06323fb7644b
Derrick Carr <derrick.the.future@gmail.com>
2016-11-30 17:02:43 -0800

    Update environment files with default host paths.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Add Mailgun config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'safewalk-dbc.herokuapp.com',
+    :authentication => :plain,
+  }
+
+  config.action_mailer.default_url_options = { host: 'safewalk-dbc.herokuapp.com' }
 end
