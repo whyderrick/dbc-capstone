b1408fc098cca7aba9050b3496097035023e4285
DL804 <darren804@gmail.com>
2016-12-01 11:14:22 -0800

    clean up code for production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,6 @@ Rails.application.configure do
   # actioncable setting for deployment to Heroku, default action is set to localhost
   config.web_socket_server_url = "wss://www.safewalk-dbc.net/cable" 
   config.action_cable.allowed_request_origins = ["http://www.safewalk-dbc.net"]
-    # config.action_cable.allowed_request_origins = ["https://safewalk-dbc.herokuapp.com"]
-
   
   # Code is not reloaded between requests.
   config.cache_classes = true
